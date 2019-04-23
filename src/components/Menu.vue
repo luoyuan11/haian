7e5b0858b47ed96e0d090fcd62627ae7e03195da
MnoKEI <2449783491@qq.com>
2019-04-23 16:27:53 +0800

    导航页，公共返回按钮

---
+++ src/components/Menu.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="menu_index">
+        <router-link to="/nav"><img src="../../static/img/nav/button.png" alt="" class="button"></router-link>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Menu'
+}
+</script>
+
+<style>
+.menu_index{
+    width:0.57rem;
+    height:0.82rem;
+    position:fixed;
+    top:0rem;
+    right:0.1rem;
+}
+.menu_index img{
+    width:0.57rem;
+    height:0.82rem;
+}
+</style>
