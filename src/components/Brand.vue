f727a1662127bc8a66ce8f8d780b73113dae6873
mi45 <597414545@qq.com>
2019-04-23 16:25:05 +0800

    修改公共样式 增加Brand页面

--- src/components/Brand.vue
+++ src/components/Brand.vue
@@ -1,3 +1,28 @@
 <template>
- <div>品牌页面 ---- 修ddd改</div>
+  <div id="brand">
+    <div class="part1">
+      <img src="../../static/img/brand/images/01.jpg" alt="">
+    </div>
+    <div class="part2">
+      <img src="../../static/img/brand/images/02.jpg" alt="">
+    </div>
+    <div class="part3">
+      <img src="../../static/img/brand/images/03_2.jpg" alt="">
+    </div>
+    <div class="part4">
+      <img src="../../static/img/brand/images/04.jpg" alt="">
+    </div>
+    <div class="part5">
+      <img src="../../static/img/brand/images/05.jpg" alt="">
+    </div>
+    <div class="part6">
+      <img src="../../static/img/brand/images/06.jpg" alt="">
+    </div>
+    <div class="part7">
+      <img src="../../static/img/brand/images/07.jpg" alt="">
+    </div>
+    <div class="part8">
+      <img src="../../static/img/brand/images/08.jpg" alt="">
+    </div>
+  </div>
 </template>
