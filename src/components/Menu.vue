afcf9ad27202316c7c713dc20c7705123cef6739
MnoKEI <2449783491@qq.com>
2019-04-24 10:33:14 +0800

    首页导航页

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -5,7 +5,7 @@
 </template>
 <script>
 export default {
-  name: 'Menu'
+  name: 'menuIndex'
 }
 </script>
 
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,29 +1,33 @@
 <template>
  <div class='box'>
-      
      <div class="bg">
-          <menuIndex></menuIndex>
         <ul class="menu">
-            <li><img src="../../static/img/nav/button_1.png" alt="" class="button1"></li>
-            <li><img src="../../static/img/nav/button_2.png" alt="" class="button2"></li>
-            <li><img src="../../static/img/nav/button_3.png" alt="" class="button3"></li>
-            <li><img src="../../static/img/nav/button_4.png" alt="" class="button4"></li>
-            <li><img src="../../static/img/nav/button_5.png" alt="" class="button5"></li>
+            <li><router-link to="/brand"><img src="/static/img/nav/button_1.png" alt=""></router-link></li>
+            <li><router-link to="/adv"><img src="/static/img/nav/button_2.png" alt=""></router-link></li>
+            <li><router-link to="/product"><img src="/static/img/nav/button_3.png" alt=""></router-link></li>
+            <li><router-link to="/inv"><img src="/static/img/nav/button_4.png" alt=""></router-link></li>
+            <li style="margin-right:0rem;"><router-link to="/support"><img src="/static/img/nav/button_5.png" alt=""></router-link></li>
         </ul>
-       <img src="../../static/img/nav/button_0.png" alt="" class="button0">
+       <img src="/static/img/nav/button_0.png" alt="" class="button0">
+       <img src="/static/img/nav/girl.png" alt="" class="girl">
     </div>
-  
  </div>
 </template>
-<script>
-import menuIndex from './Menu.vue'
-export default{
-    components:{
-        menuIndex
-    },
-}
-</script>
 <style scoped>
+@-webkit-keyframes left-in{
+    0%{left:-1.33rem}
+    100%{left:0.33rem}
+}
+@-webkit-keyframes up-down-fade{
+    0%{height:0%}
+    100%{height:1.65rem}
+}
+@-webkit-keyframes float-up-down{
+    0%{transform: translateY(0rem);}
+    25%{transform: translateY(0.05rem)}
+    75%{transform: translateY(-0.05rem)}
+    100%{transform: translateY(0rem);}
+}
  .box{
      width:3.75rem;
      height:6.67rem;
@@ -43,6 +47,15 @@ export default{
     position: absolute;
     bottom:0.4rem;
     left:1.675rem;
+    -webkit-animation: float-up-down 1s infinite linear;
+}
+.girl{
+    width:0.915rem;
+    height:1.38rem;
+    position: absolute;
+    bottom:0.36rem;
+    left:-1.33rem;
+    -webkit-animation: left-in 1s forwards 0.4s;
 }
 .menu{
     width:3.185rem;
@@ -53,12 +66,14 @@ export default{
 }
 .menu li{
     width:0.45rem;
-    height:1.17rem;
+    height:0rem;
     float:left;
+    margin-right:0.23rem;
+    overflow: hidden;
+    -webkit-animation: up-down-fade 1s forwards;
 }
 .menu li img{
     width:0.45rem;
     height:1.17rem;
-    float:left;
 }
 </style>
